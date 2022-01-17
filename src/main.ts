cd0f228152d02ae358d01c7c43222ffa4107b176
Whidy-Office <whidy@whidy.net>
2022-01-17 21:08:31 +0800

    按需导入方式

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-
-/**
- * 完整导入ElementPlus组件时，如果保持下面的顺序则出现按钮样式不正确的问题
- * 调换顺序即可解决
- */
-import 'element-plus/dist/index.css' // 这一行应该放在tailwindcss的样式之后
 import '@/assets/styles/main.css'  // 这是tailwindcss的样式
 
 import App from './App.vue'
