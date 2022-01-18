dc834b905420ac3dbbe732eb0bdaca95ee73f72d
Whidy-Office <whidy@whidy.net>
2022-01-18 15:30:00 +0800

    移除完整引入了ElementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
 import '@/assets/styles/main.css'  // 这是tailwindcss的样式
-
 import App from './App.vue'
 
 const app = createApp(App)
-
-app.use(ElementPlus)
 app.mount('#app')
