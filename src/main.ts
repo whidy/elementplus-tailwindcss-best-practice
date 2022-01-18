8c8b9e35e1e5d27543281767bb495aa9773f19ab
Whidy-Office <whidy@whidy.net>
2022-01-18 15:25:21 +0800

    修复错误的完整引入了ElementPlus问题

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
 import '@/assets/styles/modern-normalize.css'  // 这是第三方基础样式
 import '@/assets/styles/main.css'  // 这是tailwindcss的样式
 
 import App from './App.vue'
-
 const app = createApp(App)
-
-app.use(ElementPlus)
 app.mount('#app')
